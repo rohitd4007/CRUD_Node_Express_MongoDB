c1ae1dd06897c821978c0560191b80871de3441b
rohitd4007 <44453539+rohitd4007@users.noreply.github.com>
2021-06-18 12:25:19 +0530

    added database connection

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const dbConfig = require("./Config/dbConfig")
+const mongoose = require("mongoose")
 
 const app = express();
+
+
 
 
 const port = 8080;
@@ -11,3 +15,14 @@ app.listen(port, () => {
 app.get("/", (req, res) => {
     res.send("hello from backend")
 })
+
+mongoose.connect(dbConfig.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then((data) => {
+    console.log(data.Collection)
+    console.log("Successfully connected to the database");
+}).catch(err => {
+    console.log('Could not connect to the database. Exiting now...', err);
+    process.exit();
+});
